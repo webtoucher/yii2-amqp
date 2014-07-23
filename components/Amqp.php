1b9639aafbb05ada125a2419c2ca2a497c13cd41
Кузнецов Алексей <mirakuru@webtoucher.ru>
2014-07-23 14:33:51 +0700

    Add PHPDOC

--- components/Amqp.php
+++ components/Amqp.php
@@ -1,10 +1,21 @@
 <?php
+/**
+ * @link https://github.com/webtoucher/yii2-amqp
+ * @copyright Copyright (c) 2014 webtoucher
+ * @license https://github.com/webtoucher/yii2-amqp/blob/master/LICENSE.md
+ */
 
 namespace webtoucher\amqp\components;
 
 use \yii\base\Component;
 
 
+/**
+ * AMQP wrapper
+ *
+ * @author Alexey Kuznetsov <mirakuru@webtoucher.ru>
+ * @since 2.0
+ */
 class Amqp extends Component
 {
 
